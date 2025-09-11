a16c1afd12df1dbd94be7cf36c40a27b2bf26bda
guilhermetborges <guididibem@gmail.com>
2025-09-11 01:02:13 -0300

    first commit

---
+++ src/app/(public)/_components/header.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link"
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Button } from "@/components/ui/button"
+import { Menu } from "lucide-react"
+
+
+
+export function Header() {
+    return (
+        <header
+            className="fixed top-0 left-0 right-0 z-[999] py-4 px-6 bg-zinc-200">
+
+            <div className="container mx-auto flex items-center justify-between">
+                <Link href="#" className="text-2xl font-bold text-zinc-900">
+                    Nutri<span className="text-emerald-500">PRO</span>
+                </Link>
+                
+
+                <nav className="hidden md:flex items-center gap-4">
+                    <a href="#">profissionais</a>
+                </nav>
+                <Sheet >
+                    <SheetTrigger asChild className="md:hidden">  
+                        <Button className="text-black hover:bg-transparent"
+                            variant="ghost"
+                            size="icon">
+
+                            <Menu className="w-6 h-6"/>
+                        </Button>
+                    </SheetTrigger>
+                
+
+                <SheetContent side="right" className="w-[200px] sm:w-[300px] z-[999]">
+                    <SheetTitle>Menu</SheetTitle>
+                    <SheetHeader></SheetHeader>
+
+                    <SheetDescription>
+                        veja as opções
+                    </SheetDescription>
+
+                    <nav>
+                        <a href="#">profissionais</a>
+                    </nav>
+                </SheetContent>
+                </Sheet>
+            </div>
+                
+        </header>
+    )
+}
